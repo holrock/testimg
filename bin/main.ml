4e4024c102a175cab6eb2e5e50d32626ee1e3911
holrock <ispeporez@gmail.com>
2022-03-28 19:15:15 +0900

    remove line

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,4 @@
   (*
-
-    <one line to give the program's name and a brief idea of what it does.>
     Copyright (C) 2022 holrock
 
     This program is free software: you can redistribute it and/or modify
